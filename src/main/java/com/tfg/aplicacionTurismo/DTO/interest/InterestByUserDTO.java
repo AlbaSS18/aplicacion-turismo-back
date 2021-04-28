7ac8510bfd1b5089e3fb1dfbfb01414121b36513
AlbaSS18 <albaserenasuarez1998@gmail.com>
2021-04-28 18:30:30 +0200

    Change InterestByUserDTO

--- src/main/java/com/tfg/aplicacionTurismo/DTO/interest/InterestByUserDTO.java
+++ src/main/java/com/tfg/aplicacionTurismo/DTO/interest/InterestByUserDTO.java
@@ -2,6 +2,7 @@ package com.tfg.aplicacionTurismo.DTO.interest;
 
 import org.hibernate.validator.constraints.Range;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.Set;
@@ -10,7 +11,7 @@ public class InterestByUserDTO {
 
     @NotNull
     private Long interestID;
-    @NotEmpty
+    @NotBlank
     private String nameInterest;
     @NotNull
     @Range(min=0,max=10)
