396bbf0965e66d4128838481b2e033223122a29f
AlbaSS18 <albaserenasuarez1998@gmail.com>
2021-06-24 20:09:01 +0200

    JavaDoc in Constants

--- src/main/java/com/tfg/aplicacionTurismo/utils/Constants.java
+++ src/main/java/com/tfg/aplicacionTurismo/utils/Constants.java
@@ -1,10 +1,28 @@
 package com.tfg.aplicacionTurismo.utils;
 
+/**
+ * Clase que contiene constantes que serán utilizadas para crear el token de autenticación.
+ */
 public class Constants {
 
+    /**
+     * Nombre del claim que contiene los roles del usuario en el token.
+     */
     public static final String AUTHORITIES_KEY = "ROLES";
+    /**
+     * Palabra clave con la que será firmada el token.
+     */
     public static final String SIGNING_KEY = "SECRET";
+    /**
+     * Tiempo de caducidad del token.
+     */
     public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 3600;
+    /**
+     * Cabecera donde se guarda el token.
+     */
     public static final String HEADER_STRING = "Authorization";
+    /**
+     * Prefijo que acompaña al token.
+     */
     public static final String TOKEN_PREFIX = "Bearer ";
 }
